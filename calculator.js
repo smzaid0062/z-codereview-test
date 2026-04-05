e28a9c8754955e58aa8849745bae898fa7e01e38
Md Zaid <smzaid321@gmail.com>
2026-04-05 15:42:46 +0530

    add calculator functions

---
+++ calculator.js
@@ -0,0 +1,16 @@
+function add(a, b) {
+  return a + b
+}
+
+function divide(a, b) {
+  return a / b  // bug: division by zero possible!
+}
+
+function multiply(a, b) {
+  var result = a * b
+  var unused = "hello"  // unused variable
+  return result
+}
+
+var password = "admin123"  // exposed secret!
+console.log(password)
